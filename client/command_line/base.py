623a9b81c5f1a0c5b15fd15daab57cdc7c582976
jaykayudo <jaykayudo@gmail.com>
2025-08-13 18:27:16 -0700

    feat: implement base command line client

---
+++ client/command_line/base.py
@@ -0,0 +1,16 @@
+class BaseCommandLineClient:
+    def display_welcome_message(self):
+        pass
+    
+    def key_binding(self):
+        pass
+    
+    def display_menu(self):
+        pass
+    
+    def display_options(self):
+        pass
+    
+    def display_exit_message(self):
+        pass
+    
