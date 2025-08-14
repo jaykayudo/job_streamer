d3ea4ff08bc0c8eb9ae472399d2ad932770abe91
jaykayudo <jaykayudo@gmail.com>
2025-08-13 23:01:06 -0700

    chore: update base model

--- storage/core/engine.py
+++ storage/core/engine.py
@@ -11,6 +11,12 @@ SETTINGS = Settings()
 # Create database engine
 engine = create_engine(SETTINGS.DATABASE_URL, echo=True)
 
+# Create a Session factory
+SessionLocal = sessionmaker(bind=engine)
+
+# Create a session
+session = SessionLocal()
+
 
 def generate_uuid():
     return str(uuid.uuid4())
@@ -20,6 +26,28 @@ class BaseModel(DeclarativeBase):
     id = Column(String(36), primary_key=True, default=generate_uuid)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    
+    @classmethod
+    def create(cls, **kwargs):
+        obj = cls(**kwargs)
+        session.add(obj)
+        session.commit()
+        return obj
 
-# Create a Session factory
-SessionLocal = sessionmaker(bind=engine)
+    @classmethod
+    def get_or_create(cls, **kwargs):
+        obj = session.query(cls).filter_by(**kwargs).one_or_none()
+        return obj if obj else cls.create(**kwargs)
+
+    def save(self):
+        session.add(self)
+        session.commit()
+
+    def delete(self):
+        session.delete(self)
+        session.commit()
+
+
+
+
+
