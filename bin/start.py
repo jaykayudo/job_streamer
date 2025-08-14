4b3bbeb5edb2e44d75135bad014191b3a6e75a16
jaykayudo <jaykayudo@gmail.com>
2025-08-13 18:35:52 -0700

    chore: add base bin start implementation

---
+++ bin/start.py
@@ -0,0 +1,15 @@
+class BinStart:
+    def run_web(self):
+        pass
+    
+    def run_command_line(self):
+        pass
+    
+    def run(self):
+        pass
+        
+
+
+if __name__ == "__main__":
+    bin_start = BinStart()
+    bin_start.run()
