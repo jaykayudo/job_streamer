fc46533b44cb6a3ffb409f6efe590212e9862db0
jaykayudo <jaykayudo@gmail.com>
2025-08-15 23:13:21 -0700

    chore: create configuration file

---
+++ conf/configuration.py
@@ -0,0 +1,20 @@
+FULL_CONFIG = {}
+
+WELLFOUND_CONFIG = {
+    "username": "",
+    "password": "",
+}
+
+WORKABLE_CONFIG = {
+    "username": "",
+    "password": "",
+}
+
+WEB3_CAREER_CONFIG = {
+    "username": "",
+    "password": "",
+}
+
+FULL_CONFIG["wellfound"] = WELLFOUND_CONFIG
+FULL_CONFIG["workable"] = WORKABLE_CONFIG
+FULL_CONFIG["web3_career"] = WEB3_CAREER_CONFIG
