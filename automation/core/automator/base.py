294a084a747d13e720b7806e14b5bbca70fd1220
jaykayudo <jaykayudo@gmail.com>
2025-08-19 12:03:02 -0700

    feat: create automator types and functions

--- automation/core/automator/base.py
+++ automation/core/automator/base.py
@@ -6,12 +6,18 @@ from automation.core.automator.types import (
     JobDetails,
     JobFilter,
     JobApplicationDetails,
+    HiringType,
+    Industry,
+    Skill,
+    Location,
+    Category,
 )
+from utils.types import Reader, SelectionType
 
 
 class BaseAutomator(ABC):
     @abstractmethod
-    def login(self):
+    def login(self, reader: Reader):
         """
         Login to the platform
         """
@@ -24,22 +30,38 @@ class BaseAutomator(ABC):
         """
         pass
 
-    def get_categories(self) -> List[str]:
+    @abstractmethod
+    def get_categories(self) -> tuple[List[Category], SelectionType]:
         """
         Get the job categories of the platform
         """
         pass
 
-    def apply_filters(self, filters: List[JobFilter]) -> List[Job]:
+    @abstractmethod
+    def get_locations(self) -> tuple[List[Location], SelectionType]:
         """
-        Apply filters to the jobs
+        Get the locations of the platform
         """
         pass
 
     @abstractmethod
-    def search_jobs(self, query: str) -> List[Job]:
+    def get_skills(self) -> tuple[List[Skill], SelectionType] | None:
         """
-        Search for jobs on the platform
+        Get the skills of the platform
+        """
+        pass
+
+    @abstractmethod
+    def get_hiring_types(self) -> tuple[List[HiringType], SelectionType] | None:
+        """
+        Get the hiring types of the platform
+        """
+        pass
+
+    @abstractmethod
+    def get_industries(self) -> tuple[List[Industry], SelectionType] | None:
+        """
+        Get the industries of the platform
         """
         pass
 
@@ -60,19 +82,32 @@ class BaseAutomator(ABC):
         pass
 
     @abstractmethod
-    def apply_job(
-        self, job: JobDetails, application_details: List[JobApplicationDetailsAnswer]
-    ) -> bool:
-        """
-        Apply to a job
-        """
-        pass
-
-    @abstractmethod
     def get_job_application_details(
         self, job: JobDetails
     ) -> List[JobApplicationDetails]:
         """
         Get the application details of a job
+        """
+        pass
+
+    def apply_filters(self, filters: List[JobFilter]) -> List[Job]:
+        """
+        Apply filters to the jobs
+        """
+        pass
+
+    @abstractmethod
+    def search_jobs(self, query: str) -> List[Job]:
+        """
+        Search for jobs on the platform
+        """
+        pass
+
+    @abstractmethod
+    def apply_job(
+        self, job: JobDetails, application_details: List[JobApplicationDetailsAnswer]
+    ) -> bool:
+        """
+        Apply to a job
         """
         pass
